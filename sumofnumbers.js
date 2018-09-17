b97ddf0843c817945dd6d92934825dc0bf2b64db
Calvan Liang <42818741+calvan-liang@users.noreply.github.com>
2018-09-16 19:09:28 -1000

    Exercise 26

---
+++ sumofnumbers.js
@@ -0,0 +1,39 @@
+
+const testArray = [1, 2, 3];
+
+function sumFor(numbers) {
+  let total = 0;
+  for (const num of numbers) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(sumFor(testArray));
+
+function sumWhile(numbers) {
+  let total = 0;
+  let i = 0;
+  while (i < numbers.length) {
+    total += numbers[i];
+    i++;
+  }
+  return total;
+}
+
+console.log(sumWhile(testArray));
+
+function sumRecursion(numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  }
+  return numbers[0] + sumRecursion(numbers.slice(1, numbers.length));
+}
+
+console.log(sumRecursion(testArray));
+
+function sumTheSimpleWay(numbers) {
+  return _.reduce(numbers, function (memo, num) { return memo + num; }, 0);
+}
+
+console.log(sumTheSimpleWay(testArray));
